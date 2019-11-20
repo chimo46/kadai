2260895176a86a57c89461ea6587fc994833224f
chimo46 <chimo4648@gmail.com>
2019-11-20 04:53:47 +0000

    kadai

---
+++ kadai-2/index.js
@@ -0,0 +1,14 @@
+const firstName = 'minami'
+const familyName = 'otomo'
+console.log(firstName+familyName)
+
+const sum =1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const hasDog = true
+const hasCat = false
+
+console.log(hasDog||hasCat)
+console.log(hasDog&&hasCat)
+//犬も猫も飼っていない
+console.log(!hasDog&&!hasCat)
